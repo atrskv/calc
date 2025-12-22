7682f37a78ab8027b22dbcc6fa8fd3d2f29d074c
atrskv <legaterayne@gmail.com>
2025-12-22 18:39:40 +0300

    chore: initialize project

---
+++ calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Алексей Торсуков on 22.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ calcTests/calcTests.swift
@@ -0,0 +1,17 @@
+//
+//  calcTests.swift
+//  calcTests
+//
+//  Created by Алексей Торсуков on 22.12.2025.
+//
+
+import Testing
+@testable import calc
+
+struct calcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
