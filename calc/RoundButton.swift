0f340326fe0fc986c7e17736612517925ebae0bf
atrskv <legaterayne@gmail.com>
2025-12-23 00:46:43 +0300

    feat: update calculator buttons

---
+++ calc/RoundButton.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height / 2
+        layer.masksToBounds = true
+    }
+}
